a9dab475efabdf057229011b8d7c1fb245098dfa
nara <parknara0213@gmail.com>
2024-02-27 01:36:03 +0900

    240227 create page components and create routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Monthly />} />
+        <Route path="/monthly" element={<Monthly />} />
+        <Route path="/weekly" element={<Weekly />} />
+        <Route path="/daily" element={<Daily />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Weekly.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Weekly = () => {
+  return (
+    <div>
+      <h1>Weekly Page</h1>
+    </div>
+  );
+};
+
+export default Weekly;
