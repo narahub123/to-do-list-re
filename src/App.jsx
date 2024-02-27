47f2aaf67aa73a14b197bc0eac57169a2e13024e
nara <parknara0213@gmail.com>
2024-02-28 01:34:58 +0900

    240228 connect routers and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MonthlyDashboard from "./pages/MonthlyDashboard";
 import WeeklyDashboard from "./pages/WeeklyDashboard";
@@ -8,13 +9,15 @@ import Settings from "./pages/Settings";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MonthlyDashboard />} />
-        <Route path="/monthly" element={<MonthlyDashboard />} />
-        <Route path="/weekly" element={<WeeklyDashboard />} />
-        <Route path="/daily" element={<DailyDashboard />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<MonthlyDashboard />} />
+          <Route path="/monthly" element={<MonthlyDashboard />} />
+          <Route path="/weekly" element={<WeeklyDashboard />} />
+          <Route path="/daily" element={<DailyDashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Sidebar>
     </BrowserRouter>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import {
+  BsCalendarMonth,
+  BsCalendarWeek,
+  BsCalendarDate,
+  BsListCheck,
+} from "react-icons/bs";
+import { LuSettings } from "react-icons/lu";
 
 const Sidebar = () => {
+  const menuItem = [
+    {
+      path: "/",
+      name: "monthly",
+      icon: <BsCalendarMonth />,
+    },
+    {
+      path: "/",
+      name: "monthly",
+      icon: <BsCalendarWeek />,
+    },
+    {
+      path: "/",
+      name: "monthly",
+      icon: <BsCalendarDate />,
+    },
+    {
+      path: "/",
+      name: "monthly",
+      icon: <LuSettings />,
+    },
+  ];
   return (
     <div>
       <h1>sidebar</h1>
