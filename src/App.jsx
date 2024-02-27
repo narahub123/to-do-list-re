a6520804b29c15a7f05ad3a36c9cd6a41d5c2215
nara <parknara0213@gmail.com>
2024-02-28 01:27:43 +0900

    240228 create routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MonthlyDashboard from "./pages/MonthlyDashboard";
+import WeeklyDashboard from "./pages/WeeklyDashboard";
+import DailyDashboard from "./pages/DailyDashboard";
+import Settings from "./pages/Settings";
 
 const App = () => {
   return (
-    <div>
-      <h1>app.jsx</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MonthlyDashboard />} />
+        <Route path="/monthly" element={<MonthlyDashboard />} />
+        <Route path="/weekly" element={<WeeklyDashboard />} />
+        <Route path="/daily" element={<DailyDashboard />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
