be159267d2f9970e7a3ed087eea991184f775322
nara <parknara0213@gmail.com>
2024-02-28 09:18:53 +0900

    240228 toggle

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import "../App.css";
 import {
   BsCalendarMonth,
@@ -6,15 +6,20 @@ import {
   BsCalendarDate,
   BsListCheck,
 } from "react-icons/bs";
-import { LuChevronFirst } from "react-icons/lu";
+import { LuChevronFirst, LuChevronLast } from "react-icons/lu";
 import { LuSettings } from "react-icons/lu";
 import { FaRegUser } from "react-icons/fa";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { FiMoreVertical } from "react-icons/fi";
 import SidebarItem from "./SidebarItem";
 
+const SidebarContext = createContext();
 const Sidebar = ({ children }) => {
+  const location = useLocation();
+  const [alert, setAlert] = useState(true);
+  const [expanded, setExpanded] = useState(false);
+
   const menuItem = [
     {
       path: "/",
@@ -42,31 +47,48 @@ const Sidebar = ({ children }) => {
       <aside className="sidebar bg-violet-300 h-screen">
         <menu className="menu h-full flex flex-col bg-white border-r shadow-sm">
           <section className="top-section p-4 pb-2 flex justify-between items-center">
-            <div className="logo overflow-hidden transition-all flex align-center items-center w-44 ml-1">
+            <div
+              className={`logo flex align-center items-center overflow-hidden transition-all  ${
+                expanded ? "w-44 ml-1" : "w-0"
+              }`}
+            >
               <BsListCheck size={20} className="mr-2" />
-              <h1 className="text-sm">To Do List</h1>
+              <h1 className={`text-sm h-5`}>To Do List</h1>
             </div>
-            <button className="toggle p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-              <LuChevronFirst size={20} />
+            <button
+              onClick={() => setExpanded((curr) => !curr)}
+              className="toggle p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+            >
+              {expanded ? (
+                <LuChevronFirst size={20} />
+              ) : (
+                <LuChevronLast size={20} />
+              )}
             </button>
           </section>
 
-          <nav className="h-full">
-            {menuItem.map((item, index) => (
-              <NavLink
-                to={item.path}
-                key={index}
-                className="link"
-                activeclassname="active"
-              >
-                <SidebarItem icon={item.icon} text={item.name} />
-              </NavLink>
-            ))}
-          </nav>
+          <SidebarContext.Provider value={{ expanded }}>
+            <nav className="h-full">
+              {menuItem.map((item, index) => (
+                <NavLink to={item.path} key={index} className="link">
+                  <SidebarItem
+                    icon={item.icon}
+                    text={item.name}
+                    active={location.pathname === item.path}
+                    alert={alert}
+                  />
+                </NavLink>
+              ))}
+            </nav>
+          </SidebarContext.Provider>
 
           <section className="bottom-section border-t flex p-3">
             <FaRegUser className="w-10 h-10" />
-            <div className="userinfo-container flex justify-between items-center w-44 ml-3">
+            <div
+              className={`userinfo-container flex justify-between items-center overflow-hidden transition-all  ${
+                expanded ? "w-44 ml-1" : "w-0"
+              }`}
+            >
               <div className="userinfo leading-4">
                 <h4 className="username font-semibold">John Doe</h4>
                 <span className="useremail text-xs text-gray-600">
@@ -84,3 +106,4 @@ const Sidebar = ({ children }) => {
 };
 
 export default Sidebar;
+export { SidebarContext };
