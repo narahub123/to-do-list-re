76fb281112ad30bb0e0b6555e8fc11296b332ef6
nara <parknara0213@gmail.com>
2024-02-29 04:49:44 +0900

    240229 floating label

--- src/components/Sidebar/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem.jsx
@@ -5,7 +5,7 @@ const SidebarItem = ({ icon, name, active }) => {
   const { expanded } = useContext(SidebarContext);
   return (
     <li
-      className={`item relative flex items-center justify-center py-2 px-3 my-1 font=medium rounded-md cursor-pointer transition-colors ${
+      className={`item relative flex items-center justify-center py-2 px-3 my-1 font=medium rounded-md cursor-pointer transition-colors group ${
         active
           ? "bg-gradient-to-tr from-indigo-200 to-indigo-200 text-indigo-600"
           : "hover:bg-indigo-50 text-gray-600"
@@ -19,6 +19,18 @@ const SidebarItem = ({ icon, name, active }) => {
       >
         {name}
       </span>
+      {!expanded && (
+        <div
+          className={`
+            absolute left-full rounded-md px-2 py-1 ml-3
+            bg-indigo-100 text-indigo-800 text-sm
+            invisible opacity-20 translate-x-3 transition-all
+            group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+        `}
+        >
+          {name}
+        </div>
+      )}
     </li>
   );
 };
